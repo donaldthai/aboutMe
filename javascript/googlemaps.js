7b7bb237b904b6f3c4ed192617edf359ca0acb1d
Donald Thai <donald.thai@gmail.com>
2013-02-27 03:54:48 -0500

    added functions to remove, repopulate markers

--- javascript/googlemaps.js
+++ javascript/googlemaps.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
-                var map;
-                var geocoder;
-                //var marker;
+                var map; // map object
+                var geocoder; // geolocation
+                var allmarkers = []; // holds all markers
 
                 function initialize() {
                     geocoder = new google.maps.Geocoder();
@@ -13,41 +13,89 @@ $( document ).ready(function() {
                         mapTypeId: google.maps.MapTypeId.ROADMAP
                     };
                     map = new google.maps.Map(document.getElementById("map_canvas"),
-                    mapOptions);
+                        mapOptions);
                 }
 
                 function codeAddress() {
 
-                    var contentString = '<div id="mapContent">' + '<div id="siteNotice">' + '</div>' + '<h2 id="firstHeading" class="firstHeading">' + document.getElementById('address').value + '</h2>' + '<div id="bodyContent">' + '<p>Great city though!</p>' + '</div>' + '</div>';
+                    var locationComment = document.getElementById('locationComment').value;
+
+                    var contentString = '<div id="mapContent">' + 
+                                            '<div id="siteNotice">' + '</div>' + 
+                                            '<h2 id="firstHeading" class="firstHeading">' + document.getElementById('address').value + '</h2>' + 
+                                            '<div id="bodyContent">' + '<p>' + locationComment + '</p>' + '</div>' + 
+                                        '</div>';
     
                     var infowindow = new google.maps.InfoWindow({
                         content: contentString
                     });
 
                     var address = document.getElementById('address').value;
+                    
                     geocoder.geocode({
                         'address': address}, function(results, status) {
                             if (status == google.maps.GeocoderStatus.OK) {
                                 map.setCenter(results[0].geometry.location);
+                                
                                 var marker = new google.maps.Marker({
                                     map: map,
                                     position: results[0].geometry.location
                                 });
-                                //marker infobox            				
+                                
+                                //marker infobox       				
                                 google.maps.event.addListener(marker, 'click', function() {
                                     infowindow.open(map, marker);
                                 });
+                                
+                                //adding markers to marker array
+                                addMarker(marker);
                             }
                             else {
                                 alert("Geocode was not successful for the following reason: " + status);
                             }
                         });
-                }                
-                //we call the functino
-                initialize();
+                }   
                 
+                // Sets the map on all markers in the array.
+                function setAllMap(map) {
+                    for (var i = 0; i < allmarkers.length; i++) {
+                        allmarkers[i].setMap(map);
+                    }
+                }
+                
+                // Removes the overlays from the map, but keeps them in the array.
+                function clearOverlays() {
+                    setAllMap(null);
+                }
+                
+                // Shows any overlays currently in the array.
+                function showOverlays() {
+                    setAllMap(map);
+                }
+                // push marker to the array.
+                function addMarker(marker) {
+                    allmarkers.push(marker);
+                }
+                // push marker to the array.
+                function deleteAllMarkers() {
+                    clearOverlays();
+                    allmarkers = [];
+                }
+
+                //we call the functions
+                initialize();
+                //element marker, with on click function
                 $( "#marker" ).on( "click", function(event){
                     codeAddress();
+                });
+                $( "#showMarkers" ).on( "click", function(event){
+                    showOverlays();
+                });
+                $( "#clearMarkers" ).on( "click", function(event){
+                    clearOverlays();
+                });
+                $( "#deleteAllMarkers" ).on( "click", function(event){
+                    deleteAllMarkers();
                 });
                 
             });//end of jquery
