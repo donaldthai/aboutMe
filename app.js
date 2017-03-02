e7dd407a5792f5415f24881265ca9602822cbc37
Donald Thai <donald.thai@gmail.com>
2017-03-02 11:43:30 -0800

    removed appfog code in app.js

--- app.js
+++ app.js
@@ -12,39 +12,6 @@ var crypto = require('crypto');
 //creating the app
 var app = express();
 
-/*
- * Adding in MongoDB services from appfog
- * Doesn't do anything yet...
- */
-if (process.env.VCAP_SERVICES) {
-    var env = JSON.parse(process.env.VCAP_SERVICES);
-    var mongo = env['mongodb-1.8'][0]['credentials'];
-}
-else {
-    var mongo = {
-        "hostname": "localhost",
-        "port": 27017,
-        "username": "",
-        "password": "",
-        "name": "",
-        "db": "db"
-    };
-}
-var generate_mongo_url = function(obj) {
-    obj.hostname = (obj.hostname || 'localhost');
-    obj.port = (obj.port || 27017);
-    obj.db = (obj.db || 'test');
-    if (obj.username && obj.password) {
-        return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
-    }
-    else {
-        return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
-    }
-};
-var mongourl = generate_mongo_url(mongo);
-//end of MongoDB services
-
-
 //using the modules stylus and nib
 function compile(str, path) {
     return stylus(str).set('filename', path).use(nib());
@@ -61,7 +28,7 @@ app.configure(function() {
         src: __dirname + '',
         compile: compile
     }));
-    
+
     app.use(express.static(__dirname));
     //Parser for POST requests, use req.body
     app.use(express.bodyParser());
@@ -105,7 +72,7 @@ app.listen(process.env.VMC_APP_PORT || 1337, null);
 http.createServer(function (req, res) {
 
 	fs.readFile('./style.css', function (err, css) {
-		if (err) 
+		if (err)
 		{
 			throw err;
 		}
@@ -118,7 +85,7 @@ http.createServer(function (req, res) {
 	});
 
 	fs.readFile('./index.html', function (err, html) {
-		if (err) 
+		if (err)
 		{
 			throw err;
 		}
